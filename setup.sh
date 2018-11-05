4d6a8e17a7e587de692c842e29243df631368b79
Aaron Milam <aaron@milam.io>
2018-11-04 20:42:03 -0500

    Clean up old attempt at asdf installations

--- setup.sh
+++ setup.sh
@@ -13,40 +13,11 @@ $project_dir/scripts/dotfiles.sh $project_dir
 echo -e "\n${format_bold}Homebrew${format_normal}"
 $project_dir/scripts/homebrew.sh
 
+# asdf
+echo -e "\n${format_bold}asdf${format_normal}"
+$project_dir/scripts/asdf.sh
+
 exit 0
-
-echo "Installing asdf..."
-# TODO: Skip if it's already installed
-# TODO: Find or ask for the latest version number
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.0
-
-echo "Installing ruby..."
-# TODO: Skip if it's already installed
-# TODO: Ask for desired version numbers?
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby 2.4.2
-asdf use ruby 2.4.2
-gem install bundler
-asdf install ruby 2.5.1
-asdf use ruby 2.5.1
-gem install bundler
-asdf global ruby 2.5.1
-
-echo "Installing nodejs..."
-# TODO: Skip if it's already installed
-# TODO: Ask for desired version numbers?
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 10.9.0
-asdf global nodejs 10.9.0
-
-# PostgreSQL via asdf
-
-# Redis via asdf
-
-# Java via asdf
-
-# elasticsearch via asdf
 
 # Set up Github SSH
 
