a4366b8f95e79e0d84f2305f863974496f25b3e1
Aaron Milam <aaron@milam.io>
2018-11-05 20:40:22 -0500

    Minor formatting update

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,4 +1,7 @@
-echo "Checking for Homebrew..."
+format_bold=$(tput bold)
+format_normal=$(tput sgr0)
+
+echo "Checking for ${format_bold}Homebrew${format_normal}..."
 
 which -s brew
 if [[ $? != 0 ]] ; then
@@ -10,7 +13,7 @@ else
 fi
 
 # git
-echo "Checking for git via Homebrew..."
+echo "Checking for ${format_bold}git${format_normal} via Homebrew..."
 if brew ls --versions git > /dev/null; then
   echo "Git found. Updating it..."
   brew upgrade git
@@ -20,7 +23,7 @@ else
 fi
 
 # gpg
-echo "Checking for gpg via Homebrew..."
+echo "Checking for ${format_bold}gpg${format_normal} via Homebrew..."
 if brew ls --versions gpg > /dev/null; then
   echo "gpg found. Updating it..."
   brew upgrade gpg
