0df56fc98af99112f9610fa4e70b2dfaf47efe7b
Aaron Milam <aaron@milam.io>
2019-02-04 12:29:05 -0500

    Use openssl in postgres

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -64,8 +64,11 @@ if asdf which postgres > /dev/null; then
   asdf plugin-update postgres
 else
   # TODO: Ask for version(s) to install
+  export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/lib -L/usr/local/opt/expat/lib"
+  export CFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
+  export CPPFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
   asdf plugin-add postgres https://github.com/smashedtoatoms/asdf-postgres.git
-  POSTGRES_EXTRA_CONFIGURE_OPTIONS=--with-uuid=e2fs asdf install postgres 9.6.8
+  POSTGRES_EXTRA_CONFIGURE_OPTIONS="--with-uuid=e2fs --with-openssl" asdf install postgres 9.6.8
   asdf global postgres 9.6.8
 fi
 
