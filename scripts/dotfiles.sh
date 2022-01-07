9b0d645755ebedbba2339d8430efc54b378788cb
Aaron Milam <aaron@milam.io>
2022-01-07 11:02:17 -0500

    Fix symlinking

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -3,7 +3,7 @@ echo "Symlinking dotfiles..."
 project_dir=$1
 
 for filename in ${project_dir}/home/.[!.]*; do
-  ln -sv $filename ${HOME}
+  ln -sv $filename ${HOME}/
 done
 
 echo "Done with dotfiles"
